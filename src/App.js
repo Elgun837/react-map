a0944cc8342eee4fbe0ad947866db650d8b6f9ef
Elgun837 <60925597+Elgun837@users.noreply.github.com>
2024-03-11 19:26:19 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1144,8 +1144,8 @@ const App = () => {
                     icon={{
                       url: "./Vector.svg",
                       scaledSize: new window.google.maps.Size(
-                        window.innerWidth < 2000 ? 30 : 100, // Set the desired height
-                        window.innerWidth < 2000 ? 30 : 100 // Set the desired width
+                        window.innerWidth < 2000 ? 30 : 72, // Set the desired height
+                        window.innerWidth < 2000 ? 30 : 72 // Set the desired width
                       ),
                     }}
                     onClick={() => handleMarkerClick(marker)}
